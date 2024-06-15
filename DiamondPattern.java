fbfc25e4989a4174bdf6a64d412a07b79de6b919
Abhimanyu kumar <abhimanyu392kumar@gmail.com>
2024-06-16 02:37:31 +0530

    dimond pattern

---
+++ DiamondPattern.java
@@ -0,0 +1,48 @@
+
+public class DiamondPattern {
+
+    private static void printDiamond(int noOfTravel){
+        
+        int space = noOfTravel -1;
+        for (int i = 1; i < noOfTravel; i++) {
+
+            for (int j = 1; j <space ; j++) {
+                System.out.print(" ");
+            }
+            space--;
+            for (int j = 1; j <=2*i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        space = 1;
+        for (int i = 1; i < noOfTravel; i++) {
+            for (int j = 1; j <= space; j++) {
+                System.out.print(" ");
+            }
+            space++;
+            for (int j = 0; j < 2*(noOfTravel -space) -1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        printDiamond(n);
+    }
+    /*
+
+        *
+       ***
+      *****
+     *******
+      *****
+       ***
+        *
+
+
+     */
+}
+
